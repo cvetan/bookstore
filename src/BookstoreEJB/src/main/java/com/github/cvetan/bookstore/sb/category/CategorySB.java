62005f2cf12a3bdd1ff76f8b86b7e5dc649ad193
Cvetan Šimšić <cvetan.simsic@gmail.com>
2018-08-03 00:09:53 +0200

    Corrected properties files. Changed default license headers in project settings.

--- src/BookstoreEJB/src/main/java/com/github/cvetan/bookstore/sb/category/CategorySB.java
+++ src/BookstoreEJB/src/main/java/com/github/cvetan/bookstore/sb/category/CategorySB.java
@@ -2,8 +2,6 @@ package com.github.cvetan.bookstore.sb.category;
 
 import com.github.cvetan.bookstore.model.Category;
 import com.github.cvetan.bookstore.sb.BookstoreSB;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import javax.ejb.Stateless;
 
@@ -16,29 +14,7 @@ public class CategorySB extends BookstoreSB implements CategorySBLocal {
 
     @Override
     public List<Category> getList() {
-        List<Category> rawList = entityManager.createNamedQuery("Category.findAll").getResultList();
-//        List<Category> formatedList = new ArrayList<>();
-//        
-//        formatList(rawList, formatedList, 0);
-//        
-//        return formatedList;
-        return rawList;
-    }
-    
-    private void formatList(List<Category> rawList, List<Category> formatedList, int level) {
-        for (Category c: rawList) {
-            if (formatedList.contains(c)) {
-                continue;
-            }
-            
-            c.setName(String.join("", Collections.nCopies(level, " - ")));
-            
-            formatedList.add(c);
-            
-            if ( ! c.getChildren().isEmpty()) {
-                formatList(c.getChildren(), formatedList, level + 1);
-            }
-        }
+        return entityManager.createNamedQuery("Category.findAll").getResultList();
     }
 
     @Override
